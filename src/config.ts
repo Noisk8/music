47cb0c9d214a4b68e76a0e64a2ac585d7a0456fd
Noisk8 <juansk8jaramillo@gmail.com>
2025-02-03 22:23:21 -0500

    add about me

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,14 @@
 
 
 export const SITE_FAVICON = "/favicon.png";
-export const SITE_LOGO = "/leo.png";
+export const SITE_LOGO = "https://www.noisk8.xyz/assets/noi.jpg";
 export const SITE_TITLE = "NOISK8";
 export const SITE_URL = "https://astro-theme-leo.muji.dev";
 export const SITE_DESCRIPTION =
   `
   This is a space where creativity meets logic. As a passionate frontend engineer and dreamer inspired by the vastness of space, Leo shares insights on cutting-edge web development, personal projects, and the exploration of new technologies. With a unique blend of technical expertise and a love for innovation, this blog is a journey into problem-solving, design, and the limitless possibilities of the digital universe.
 `;
-export const ME_AVATAR = "/leo.png";
+export const ME_AVATAR = "https://www.noisk8.xyz/assets/noi.jpg";
 export const LANGUAGES = ['en', 'zh'];
 
 export const MENUS = [
@@ -24,68 +24,62 @@ export const MENUS = [
   },
   {
     title: "Github",
-    link: "YOUR_GITHUB_URL",
+    link: "https://github.com/Noisk8/",
     target: "_blank",
     icon: "github",
   },
 ];
 
-export const FOOTER_CONTENT = "Mr.Leo. All rights reserved.";
+export const FOOTER_CONTENT = "Noisk8";
 
 export const CONTACTS = {
-  linkedIn: "YOUR_LINKEDIN_URL",
-  github: "YOUR_GITHUB_URL",
-  email: "mailto:YOUR_EMAIL",
+  linkedIn: "https://co.linkedin.com/in/juan-jaramillo-silva-689798267",
+  github: "https://github.com/Noisk8/",
+  email: "mailto:noisk8@proton.me",
 };
 
 export const SLOGAN = "Musica Rudio Y livecoding";
 export const ME_DESCRIPTION =
-  `Hola Soy NOisk8, soy un apasionado de la musica y el sonido, también soy un total enamorado de las tecnologías de codigo abierto, considero que la tecnología y el conocimiento debe ser compartido en su mayoir expresión, es por eso que para mis producciones utilizo herramientas libres Livecoding y sintetizadores DIY `;
+  `Hola Soy Noisk8, soy un apasionado de la musica y el sonido, también soy un total enamorado de las tecnologías de codigo abierto, considero que la tecnología y el conocimiento debe ser compartido en su mayoir expresión, es por eso que para mis producciones utilizo herramientas libres Livecoding y sintetizadores DIY `;
 
 
 export const WORK_EXPERIENCE = [
   {
-    company: "Galactic Ventures",
+    company: "Kabalah Records",
     position: "Innovation Explorer",
-    startDate: "2022",
+    startDate: "2024",
     endDate: "2024",
   },
   
   {
-    company: "Interstellar Academy Program",
+    company: "Nucleoroto",
     position: "Astro Trainee",
     startDate: "2023",
     endDate: "2023",
   },
   
   {
-    company: "LunarTech Solutions",
+    company: "Corralon Records",
     position: "Creative Logic Engineer",
     startDate: "2020",
     endDate: "2022",
   },
   
-  {
-    company: "Cosmic Coders Inc.",
-    position: "Frontend Navigator",
-    startDate: "2018",
-    endDate: "2020",
-  },
+
   
 ];
 
 
 export const PROJECTS = [
     {
-      id: "my-first-rocket-launch",
-      title: "My First Rocket Launch",
-      description: "This is the first rocket launch I participated in. I was a member of the Interstellar Academy Program, and I was responsible for the rocket's navigation system. I was able to successfully navigate the rocket to the target destination.",
+      id: "Showme the code",
+      title: "Show Me  the code",
+      description: "Me gusta compartir  el código de m is canciones y presentaciones en vivo, en este espacio podrás encontrar el código fuente de mis proyectos y present",
       image: 'https://images.unsplash.com/photo-1516849841032-87cbac4d88f7?q=80&w=2370&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      externalLink: "https://www.nasa.gov/",
+      externalLink: "https://gist.github.com/Noisk8",
       details: [
-        "Explaining space exploration concepts through creative illustrations.",
-        "Making it easier for aspiring explorers to understand the basics of interstellar travel.",
-        "Won an award in the interstellar creativity competition.",
+        "Codigos para usar en FoxDot (Python)",
+      
       ],
     },
   ];
